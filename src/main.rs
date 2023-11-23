f2f7f407e36381ac1473d136c2b083c3ccf05480
Gerard Fabregó <gerardfabrego@gmail.com>
2023-11-23 13:18:59 +0100

    Display cube instead of square

--- src/main.rs
+++ src/main.rs
@@ -90,14 +90,47 @@ fn main() {
     let (surface, mut config, device, queue) = pollster::block_on(wgpu_init(&window));
 
     #[rustfmt::skip]
-    let vertices: [Vertex; 4] = [
-        Vertex {position: [-0.5, -0.5, 0.0], color: [1.0, 0.0, 0.0] },
-        Vertex {position: [ 0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
-        Vertex {position: [ 0.5,  0.5, 0.0], color: [0.0, 0.0, 1.0] },
-        Vertex {position: [-0.5,  0.5, 0.0], color: [0.0, 1.0, 0.0] },
+    let vertices: [Vertex; 24] = [
+
+        Vertex {position: [-1.0, -1.0,  1.0], color: [1.0, 0.0, 0.0] },
+        Vertex {position: [ 1.0, -1.0,  1.0], color: [1.0, 0.0, 0.0] },
+        Vertex {position: [ 1.0,  1.0,  1.0], color: [1.0, 0.0, 0.0] },
+        Vertex {position: [-1.0,  1.0,  1.0], color: [1.0, 0.0, 0.0] },
+
+        Vertex {position: [-1.0,  1.0, -1.0], color: [0.0, 1.0, 0.0] },
+        Vertex {position: [ 1.0,  1.0, -1.0], color: [0.0, 1.0, 0.0] },
+        Vertex {position: [ 1.0, -1.0, -1.0], color: [0.0, 1.0, 0.0] },
+        Vertex {position: [-1.0, -1.0, -1.0], color: [0.0, 1.0, 0.0] },
+
+        Vertex {position: [ 1.0, -1.0, -1.0], color: [0.0, 0.0, 1.0] },
+        Vertex {position: [ 1.0,  1.0, -1.0], color: [0.0, 0.0, 1.0] },
+        Vertex {position: [ 1.0,  1.0,  1.0], color: [0.0, 0.0, 1.0] },
+        Vertex {position: [ 1.0, -1.0,  1.0], color: [0.0, 0.0, 1.0] },
+
+        Vertex {position: [-1.0, -1.0,  1.0], color: [1.0, 1.0, 0.0] },
+        Vertex {position: [-1.0,  1.0,  1.0], color: [1.0, 1.0, 0.0] },
+        Vertex {position: [-1.0,  1.0, -1.0], color: [1.0, 1.0, 0.0] },
+        Vertex {position: [-1.0, -1.0, -1.0], color: [1.0, 1.0, 0.0] },
+
+        Vertex {position: [ 1.0,  1.0, -1.0], color: [1.0, 0.0, 1.0] },
+        Vertex {position: [-1.0,  1.0, -1.0], color: [1.0, 0.0, 1.0] },
+        Vertex {position: [-1.0,  1.0,  1.0], color: [1.0, 0.0, 1.0] },
+        Vertex {position: [ 1.0,  1.0,  1.0], color: [1.0, 0.0, 1.0] },
+
+        Vertex {position: [ 1.0, -1.0,  1.0], color: [0.0, 1.0, 1.0] },
+        Vertex {position: [-1.0, -1.0,  1.0], color: [0.0, 1.0, 1.0] },
+        Vertex {position: [-1.0, -1.0, -1.0], color: [0.0, 1.0, 1.0] },
+        Vertex {position: [ 1.0, -1.0, -1.0], color: [0.0, 1.0, 1.0] },
     ];
 
-    let indices: [u32; 6] = [0, 1, 2, 2, 3, 0];
+    let indices: [u32; 36] = [
+        0, 1, 2, 2, 3, 0, // top
+        4, 5, 6, 6, 7, 4, // bottom
+        8, 9, 10, 10, 11, 8, // right
+        12, 13, 14, 14, 15, 12, // left
+        16, 17, 18, 18, 19, 16, // front
+        20, 21, 22, 22, 23, 20, // back
+    ];
 
     let vertex_buffer_layout = wgpu::VertexBufferLayout {
         array_stride: size_of::<Vertex>() as u64,
@@ -127,7 +160,7 @@ fn main() {
 
     let index_buffer = device.create_buffer(&wgpu::BufferDescriptor {
         label: Some("Index buffer"),
-        size: (size_of::<u32>() * 6) as u64,
+        size: (size_of::<u32>() * indices.len()) as u64,
         usage: wgpu::BufferUsages::INDEX | wgpu::BufferUsages::COPY_DST,
         mapped_at_creation: false,
     });
@@ -135,7 +168,7 @@ fn main() {
     queue.write_buffer(&index_buffer, 0, bytemuck::cast_slice(&indices));
 
     let camera = Camera::new(
-        (0.0, 0.0, 1.0).into(),
+        (0.0, 2.0, 4.0).into(),
         (0.0, 0.0, 0.0).into(),
         config.width as f32 / config.height as f32,
         45.0,
@@ -144,8 +177,6 @@ fn main() {
     );
 
     let camera_uniform_data: [[f32; 4]; 4] = camera.get_view_projection_matrix().into();
-
-    println!("HERE: {:?}", camera_uniform_data);
 
     let camera_uniform_buffer = device.create_buffer(&wgpu::BufferDescriptor {
         label: Some("Camera uniform buffer"),
@@ -286,7 +317,7 @@ fn main() {
                         render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
                         render_pass
                             .set_index_buffer(index_buffer.slice(..), wgpu::IndexFormat::Uint32);
-                        render_pass.draw_indexed(0..6, 0, 0..2);
+                        render_pass.draw_indexed(0..indices.len() as u32, 0, 0..2);
 
                         drop(render_pass);
 
